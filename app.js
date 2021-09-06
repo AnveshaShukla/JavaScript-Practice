b5a14c45f52bba5a28b25ac71d9589d5e85a1ecc
AnveshaShukla <as9334@srmist.edu.in>
2021-09-06 21:29:27 +0530

    data types

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-console.log("This is my first piece of js code")
-console.log("12245")
-console.log({name:"Anvesha"})
-console.warn("This is a warning")
-console.error("This is an error")
+//data types
+// primitive data types
+var Name="Anvesha Shukla"
+console.log(Name)
+let age=19
+console.log(typeof age)
+let Namee="Anvesha"
+console.log(Namee)
+let brand=null
+console.log(brand)
+let isTrue=true
+console.log(isTrue)
+console.log(typeof isTrue)
+
+//non primitive data type
+
+let arr=[1,2,3,4,5]
+console.log(arr)
+arr.push(6) //pushes another element to the end of array
+console.log(arr)
+arr.push("anvesha")
+console.log(arr) //not limited to one data type 
+arr.unshift(0)//adds an element to the beginning of an array
+console.log(arr) 
