4475466f10214a01e507a51b0c2ccbfd71b5930b
AnveshaShukla <as9334@srmist.edu.in>
2021-09-07 03:39:37 +0530

    Important-various methods

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-// const element=document.getElementById("selectme")
-// const element=document.getElementsByClassName('heading')
-// const element=document.getElementsByTagName('h2')
-const element=document.querySelectorAll('h3.heading')
-console.log(element)
+const element=document.querySelector("#selectme")
+// element.innerHTML="This is from JS"
+// element.style.color="cyan"
+// element.style.fontSize="10px"
+// element.style="display:none"
+// console.log(element.classList)
+// element.classList.add('five')
+// console.log(element.classList)
+// element.classList.remove("one")
+// console.log(element.classList)
+const body=document.querySelector('body')
+console.log(body)
+console.log(body.classList.toggle('dark'))
+const value = element.classList.contains('one')
+console.log(value)
