aedc80d2ca8dddcfe9f2af93eac9176ba06ff7f3
AnveshaShukla <as9334@srmist.edu.in>
2021-09-07 01:19:23 +0530

    Arithmetic operators

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
-//data types
-// primitive data types
-var Name="Anvesha Shukla"
-console.log(Name)
-let age=19
-console.log(typeof age)
-let Namee="Anvesha"
-console.log(Namee)
-let brand=null
-console.log(brand)
-let isTrue=true
-console.log(isTrue)
-console.log(typeof isTrue)
+// // arithmetic operators------------
 
-//non primitive data type
-
-let arr=[1,2,3,4,5]
-console.log(arr)
-arr.push(6) //pushes another element to the end of array
-console.log(arr)
-arr.push("anvesha")
-console.log(arr) //not limited to one data type 
-arr.unshift(0)//adds an element to the beginning of an array
-console.log(arr) 
+// const age=19
+// console.log(age)
+const a=2
+const b=15
+c=10
+console.log(a+b)
+console.log(a-b)
+c+=1
+console.log(c++)
+console.log(c++)
