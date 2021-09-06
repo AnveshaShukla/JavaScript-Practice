02e4f7a80a1bf05fe193886e6b87ede9f0cdc2dd
AnveshaShukla <as9334@srmist.edu.in>
2021-09-07 02:03:09 +0530

    Control statements

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-// // arithmetic operators------------
-
-// const age=19
-// console.log(age)
-const a=2
-const b=15
-c=10
-console.log(a+b)
-console.log(a-b)
-c+=1
-console.log(c++)
-console.log(c++)
+// Control Statements----------------------------
+//odd or even
+const a=0
+if(typeof a==='number'){
+ 
+    if(a===0){
+        console.log("Neither even nor odd")
+    }
+ else if(a%2===0)
+{
+   console.log("Even")
+}
+else{
+    console.log("Odd")
+}
+}
+else{
+    console.error("Not a number")
+}
