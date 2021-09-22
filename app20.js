ecfb3c6a53c2ec6bc6d42076a3985aab404cf377
AnveshaShukla <as9334@srmist.edu.in>
2021-09-23 02:59:51 +0530

    Async and await, try and catch

--- app20.js
+++ app20.js
@@ -1,12 +1,42 @@
-console.log("MERGED CLASS 2 ES6")
+// console.log("MERGED CLASS 2 ES6")
 
-let ranks=[1,2,3,4,5,6];
-// const newArr = ranks.map((e)=>e+1);
+// let ranks=[1,2,3,4,5,6];
+// const newArr = ranks.map((e)=>e+1);  //Map method
 // console.log(newArr);
 
-let newfilterarr=ranks.filter((e, index) =>
+// let newfilterarr=ranks.filter((e, index) =>  //Filter method
+// {
+//   if(e%2===0)
+//     return e;
+// })
+// console.log(newfilterarr);
+
+let concert=false;     //PROMISE
+let attendConcert = new Promise(function (resolve, reject) {
+    setTimeout(() =>{
+    if(concert){
+        resolve("BOB ATTENDED THE EVENT");
+    }
+    else{
+        reject("BOB FAILED TO ATTEND THE EVENT");
+    }
+    }, 2000);
+});
+console.log(attendConcert);
+
+// attendConcert.then((data) =>console.log(data));  //then method, you can  use any variable
+// attendConcert.catch((error) =>console.log(error));  //catch method you can  use any variable
+
+const func=async()=>                //async and await;try and catch
 {
-  if(e%2===0)
-    return e;
-})
-console.log(newfilterarr);
+    try{
+        let result=await attendConcert;
+        console.log(result);
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+func()
+console.log("function is:", func())
+ 
